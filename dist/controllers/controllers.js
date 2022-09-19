e109c7f597c73109c97a08837c12e684f4839bec
Joshua Binns <joshuabinns.coding@gmail.com>
2022-09-19 10:36:35 +0100

    Added endpoints and a test endpoint

---
+++ dist/controllers/controllers.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TestsController = void 0;
+exports.TestsController = require('./test.controller');
+//# sourceMappingURL=controllers.js.map
--- dist/src/app.js
+++ dist/src/app.js
@@ -5,10 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const TestController = require('../controllers/test.controller');
+const { Endpoint, endpoints } = require('./endpoints');
 const app = (0, express_1.default)();
-const port = 3001;
+const port = 3000;
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+endpoints.filter((endpoint) => endpoint.method === 'GET').forEach((endpoint) => {
+    app.get(endpoint.endpoint, [
+        endpoint.controller
+    ]);
 });
 app.get('/tests', [
     TestController.tests
